8525736991a5839234dc9791eb72c3cb34834959
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-05 16:47:49 +0000

    refactor ConnectionPool

--- src/model/ConnectionPool.java
+++ src/model/ConnectionPool.java
@@ -7,10 +7,12 @@ public class ConnectionPool {
     private static ObjectContainer db;
 
     public static ObjectContainer getConnection(String fileName) {
-        return db = Db4o.openFile(fileName);
+        return db == null ? db = Db4o.openFile(fileName) : db;
     }
 
+    /* La conexión debe mantenerse abierta
     public static void closeConnection() {
         db.close();
     }
+    */
 }
