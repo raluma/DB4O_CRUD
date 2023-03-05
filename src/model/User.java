9b3e3cf70e7151373a787be648fff627cdba8e97
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-05 16:55:44 +0000

    creating user class

---
+++ src/model/User.java
@@ -0,0 +1,57 @@
+package model;
+
+public class User {
+    private final int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    public User() {
+        this.id = 0;
+    }
+
+    public User(int id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
