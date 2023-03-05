44b7b34ee7889df4cda804bc3ebfc4711e643eee
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-05 16:54:32 +0000

    creating Login view

---
+++ src/view/Login.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    private JPanel window;
+    private JTextField userTxt;
+    private JPasswordField passTxt;
+    private JButton signupBtn;
+    private JButton loginBtn;
+
+    public Login() {
+        super();
+        setContentPane(this.window);
+    }
+
+    public JTextField getUserTxt() {
+        return userTxt;
+    }
+
+    public JPasswordField getPassTxt() {
+        return passTxt;
+    }
+
+    public JButton getSignupBtn() {
+        return signupBtn;
+    }
+
+    public JButton getLoginBtn() {
+        return loginBtn;
+    }
+}
